acf3677545abebed7deb799faae93bc92bb1c908
sumerapunekar <114940293+sumerapunekar@users.noreply.github.com>
2022-10-30 10:10:09 +0530

    Add files via upload

---
+++ bank.js
@@ -0,0 +1,17 @@
+// let User = {
+//   name : "Anmol Raj",
+//   age : 18 ,
+//   phoneNumber : 82454954165,
+//   address : "Hyderabad , telangana India",
+//   pin : 8789,
+//   currBal : 500 ,
+//   changeName : function (newName){ this.name = newName} ,
+//   changeAge : function (newAge){this.age = newAge} ,
+//   changePin : function (newPin){ this.pin = newPin} ,
+//   printDetails : function () {return { name : this.name , age :this.age ,address : this.address }},
+//   creditBalance : function (x) { this.currBal = this.currBal +  x ; return this.currBal;} ,
+//   debitBalance : function (x) { this.currBal = this.currBal -  x },
+//   checkBalance : function () { return this.currBal}
+// };
+
+// document.getElementById("b1").value=0
